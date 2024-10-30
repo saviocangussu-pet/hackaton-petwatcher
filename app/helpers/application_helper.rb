bae226b86db0ba49cde7139777a28840c4587aad
Franco Rodriguez <franco.rodriguez@dualbootpartners.com>
2024-10-30 17:31:44 +0100

    [feature] some FE work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def nav_link(name, path, options = {})
+    class_name = current_page?(path) ? 'nav-link font-weight-bold' : 'nav-link'
+    link_to name, path, { class: class_name }.merge(options)
+  end
 end
