6288171edbcdb2309fef50a2d3d332afefe40d65
Franco Rodriguez <franco.rodriguez@dualbootpartners.com>
2024-10-30 19:04:08 +0100

    [feature] dashboard fe work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < ApplicationController
   before_action :configure_person
 
   def dashboard
-    @closest_sitters = @person.closest_sitters.includes(sitter_profile: :species)
+    #Todo: we need to improve this query, i receive only 1 with 0 kms away, when we have 2 users with 0km away
+    @closest_sitters = @person.closest_sitters
+                              .includes(sitter_profile: :species)
+                              .sort_by { |sitter| [sitter.distance, sitter.id] }
+                              .last(6)
+    @sitters = @person.closest_sitters.includes(sitter_profile: :species)
   end
 end
